1931f6609934d91fc755c025450d45964cd4530f
bakugod <47696795+bakugod@users.noreply.github.com>
2019-09-13 21:08:33 +0300

    Add files via upload

---
+++ Interface1.cs
@@ -0,0 +1,29 @@
+﻿namespace _Number
+{
+    using System;
+
+    abstract class Number
+    {
+        public abstract int Constructor();
+    }
+
+    class Numeric : Number
+    {
+        static int Decimal; 
+
+        public static int Getter(int n) => Decimal = n;
+
+        public override int Constructor() => Decimal;
+
+        
+        static void Main()
+        {
+
+            var sss = new Getter(12);
+
+            Console.WriteLine($"Area of the square = {sss.Constructor()}");
+        }
+    }
+
+}
+
