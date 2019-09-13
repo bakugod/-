39e9aa71218a32ee77fad26c41de028162f77434
bakugod <47696795+bakugod@users.noreply.github.com>
2019-09-13 21:09:07 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace octoCat
+{
+    using System;
+    class Program
+    {
+        
+        public static long Test(int x)
+        {
+            var i = x * x;
+            return i;
+        }
+        
+        private static void Main2()
+        {
+            
+
+            Console.WriteLine(Test(10));
+        }
+    }
+}
